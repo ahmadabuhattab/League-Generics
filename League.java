24e262b17f8cfa4278c9d461649433b678865da3
Ahmad Abu Hattab <101012392+ahmadabuhattab@users.noreply.github.com>
2022-09-07 00:39:41 -0400

    Add files via upload

---
+++ League.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class League <T extends Team> {
+    private ArrayList<T> league = new ArrayList<T>();
+    public String name;
+
+    public League(String name) {
+        this.name = name;
+    }
+    public boolean add(T team){
+        if (league.contains(team)){
+            return false;
+        }
+        league.add(team);
+        return true;
+    }
+    public void showLeagueTable(){
+        Collections.sort(league);
+        for (T t : league){
+            System.out.println(t.getName() + ": " + t.ranking());
+        }
+    }
+}
+// Create a generic class to implement a league table for a sport.
+// The class should allow teams to be added to the list, and store
+// a list of teams that belong to the league.
+//
+// Your class should have a method to print out the teams in order,
+// with the team at the top of the league printed first.
+//
+// Only teams of the same type should be added to any particular
+// instance of the league class - the program should fail to compile
+// if an attempt is made to add an incompatible team.
